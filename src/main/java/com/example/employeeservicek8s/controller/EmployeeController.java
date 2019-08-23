b02ee764286cb0239d1491fe88431f554f52cd0d
Partha <partharecrkgp@gmail.com>
2019-08-23 15:18:29 +0530

    jj

--- src/main/java/com/example/employeeservicek8s/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservicek8s/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.employeeservicek8s.model.Employee;
@@ -18,7 +19,7 @@ import com.example.employeeservicek8s.repository.EmployeeRepository;
 
 
 @RestController
-//@RequestMapping(value = "/employee")
+@RequestMapping(value = "/employee")
 public class EmployeeController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
